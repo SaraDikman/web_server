1bf3fbb12b41fca2328f45c3d413206c7d3e95dd
SaraDikman <sararom1000@gmail.com>
2024-08-11 14:44:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import http.server
 import socketserver
 
-PORT = 80
+PORT = 8080
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        print("Received GET request")
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"<!DOCTYPE html><html><head><title>Welcome</title></head><body><h1>Welcome</h1></body></html>")
-
+        self.wfile.write(b"<!DOCTYPE html><html><head><title>Welcome</title></head><body><h1>!!Welcome!!</h1></body></html>")
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
     print("Serving at port", PORT)
